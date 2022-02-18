06fe046b229a0c61702bb7db2a5a42fa3444cd13
liji356 <lijibthomas95@gmail.com>
2022-02-18 13:29:28 +0530

    upto menu activity

--- app/src/main/java/com/example/courseapp/MainActivity.java
+++ app/src/main/java/com/example/courseapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -23,7 +24,7 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     EditText ed1,ed2,ed3,ed4,ed5;
-    AppCompatButton b1;
+    AppCompatButton b1,b2;
 
     String apiurl="https://mountzioncollege.herokuapp.com/addcourse";
 
@@ -39,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         ed4=(EditText) findViewById(R.id.date);
         ed5=(EditText) findViewById(R.id.duration);
         b1=(AppCompatButton) findViewById(R.id.submit);
+        b2=(AppCompatButton) findViewById(R.id.menu);
 
 
         b1.setOnClickListener(new View.OnClickListener() {
@@ -94,6 +96,14 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i=new Intent(getApplicationContext(),MenuActivity.class);
+                startActivity(i);
+            }
         });
     }
 }
